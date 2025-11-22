2d0928ac38181d3a6b5e70d1ef8e5a161c7ff91b
vivekVaddepally <vaddevivek19@gmail.com>
2025-11-22 14:22:50 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MOMBadge from "@/components/ui/MOMBadge";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <SpeedInsights />
         <MOMBadge />
       </body>
