faad3caf8542e52f764b72331c470354de519b92
vivekVaddepally <vaddevivek19@gmail.com>
2025-11-22 14:19:59 +0530

    speed cal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MOMBadge from "@/components/ui/MOMBadge";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
         <MOMBadge />
       </body>
     </html>
